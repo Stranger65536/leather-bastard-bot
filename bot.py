aab73a1921ef8fc086a4038f448a5cd6a12ad90b
Vladislav Trofimov <stranger.65536@gmail.com>
2021-08-03 04:38:48 +0300

    improvements

--- bot.py
+++ bot.py
@@ -1,23 +1,20 @@
 """
 Leather bastards python bot
 """
+import subprocess
 from contextlib import closing
+from datetime import datetime
 from json import load
 from logging import basicConfig, INFO, getLogger, info
-from os import write, close, remove
-from subprocess import Popen, PIPE
-from tempfile import mkstemp
+from os import remove
+from os.path import dirname, realpath, join
 from time import sleep
 
 from boto3 import client
-from requests import get
 # noinspection PyPackageRequirements
 from telegram import Bot
 from telegram.ext import Updater, CommandHandler, \
     MessageHandler, Filters
-from validators import url as validate_url
-from os.path import dirname, realpath, join
-
 
 AWS_KEY = 'your_token_here'
 AWS_SECRET_ACCESS_KEY = 'your_secret_access_key_here'
@@ -69,46 +66,12 @@ def help_command(update, context):
                               'play it on device')
 
 
-def get_aws_function(text):
-    """
-    Reqests open API to get url to AWS function that produces audio
-    :param text: text to speech
-    """
-    try:
-        response = get("https://nextup.com/ivona/php/nextup-polly"
-                       "/CreateSpeech/CreateSpeechGet3.php",
-                       allow_redirects=True,
-                       params={
-                           "voice": "Maxim",
-                           "language": "ru-RU",
-                           "text": text
-                       })
-        response.raise_for_status()
-        validate_url(response.text)
-        return response.text
-    except Exception as e:
-        raise ApiRequestError("API call error!", e)
-
-
-def get_audio_by_url(aws_url):
-    """
-    saves audio produced by aws function to temp file
-    """
-    try:
-        response = get(aws_url, allow_redirects=True)
-        response.raise_for_status()
-        fd, f = mkstemp(prefix="leather-bastard-")
-        write(fd, response.content)
-        return fd, f
-    except Exception as e:
-        raise AwsFunctionError("API call error!", e)
-
-
 def aws_call(text):
     """
     saves audio produced by aws function to temp file
     """
-    response = polly.synthesize_speech(Text=text,
+    response = polly.synthesize_speech(Text=f"<speak>{text}</speak>",
+                                       TextType="ssml",
                                        OutputFormat="mp3",
                                        SampleRate="24000",
                                        LanguageCode="ru-RU",
@@ -120,23 +83,22 @@ def aws_call(text):
         # Here we are using contextlib.closing to ensure the close
         # method of the stream object will be called automatically
         # at the end of the with statement's scope.
+        tmp_dir = dirname(realpath(__file__))
+        now = str(datetime.now()).replace(" ", "_")
+        file = f"{tmp_dir}/leather-bastard-{now}"
         with closing(response["AudioStream"]) as stream:
-            fd, f = mkstemp(prefix="leather-bastard-")
-            write(fd, stream.read())
-            return fd, f
+            arr = stream.read()
+            with open(file, mode="wb") as f:
+                f.write(arr)
+        return file
     else:
         raise AwsFunctionError(response)
 
 
-def cleanup(fd, f):
+def cleanup(f):
     """
     Cleans up files
     """
-    # noinspection PyBroadException
-    try:
-        close(fd)
-    except Exception:
-        pass
     # noinspection PyBroadException
     try:
         remove(f)
@@ -144,9 +106,13 @@ def cleanup(fd, f):
         pass
 
 
+sound = "1.0"
+
+
 # noinspection PyUnusedLocal
 def echo(update, context):
     """Echo the user message."""
+    global sound
     info(update.message)
     with open(join(dir_name, "whitelist.json"), "r") as f:
         whitelist = load(f)
@@ -161,7 +127,13 @@ def echo(update, context):
                         .format(update.message.from_user.name))
         update.message.reply_text("Pong")
         return
-    fd = None
+    if update.message.text.strip() == 'sound':
+        update.message.reply_text(text="sound: " + sound)
+        return
+    if update.message.text.strip().startswith("sound"):
+        sound = update.message.text.strip().split(" ")[-1]
+        update.message.reply_text(text="sound: " + sound)
+        return
     f = None
     try:
         text = update.message.text
@@ -170,16 +142,17 @@ def echo(update, context):
                         .format(update.message.from_user.name, text))
         if text is None:
             update.message.reply_text("I work only with text :(")
-        # url = get_aws_function(text)
-        fd, f = aws_call(text)
-        process = Popen(["play", "-v", "4.0", "-t", "mp3", f],
-                        stdout=PIPE,
-                        stderr=PIPE)
-        process.wait()
+        f = aws_call(text)
+        # noinspection PyBroadException
+        res = subprocess.run(["play", "-v", sound, "-t", "mp3", f],
+                             check=True, shell=False)
+        with open(f, "rb") as f:
+            bot.sendAudio(chat_id=ADMIN_CHAT_ID, audio=f,
+                          reply_to_message_id=update.message.message_id)
     except Exception as e:
         update.message.reply_text("Oops! {}".format(e))
     finally:
-        cleanup(fd, f)
+        cleanup(f)
 
 
 def main():
